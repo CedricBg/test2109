a2c3443210681b5ce380c306720660f055ece0fb
bogaert cedric <bogaert@outlook.com>
2023-02-22 23:24:00 +0100

    Ajout adress api

---
+++ test2109/Models/Address.cs
@@ -0,0 +1,17 @@
+﻿namespace test2109.Models
+{
+    public class Address
+    {
+        public int Id { get; set; }
+
+        public string? SreetAddress { get; set; }
+
+        public string? City { get; set; }
+
+        public string? State { get; set; }
+
+        public int? StateId { get; set; }
+
+        public string? ZipCode { get; set; }
+    }
+}
